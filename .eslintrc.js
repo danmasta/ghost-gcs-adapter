75e0c0afd905fe7aaa25a71d7534a22c6ee2f5d9
Daniel Smith <dannmasta@gmail.com>
2020-07-29 23:00:11 -0700

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            { "SwitchCase": 1 }
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": [
+            "warn"
+        ],
+        "no-undef": [
+            "off"
+        ],
+        "no-unused-vars": [
+            1
+        ]
+    }
+};
