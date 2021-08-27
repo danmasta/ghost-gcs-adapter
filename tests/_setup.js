101f1b4b5b9d19028d429f26ea1517b9506b6e9f
Daniel Smith <dannmasta@gmail.com>
2021-08-27 15:09:54 -0700

    add mock image objects for tests

--- tests/_setup.js
+++ tests/_setup.js
@@ -1,7 +1,36 @@
 const chai = require('chai');
+const GhostGCSAdapter = require('../index');
 
 beforeEach(() => {
     global.assert = chai.assert;
     global.expect = chai.expect;
     global.should = chai.should();
+    global.images = {
+        test: {
+            fieldname: 'file',
+            originalname: 'test.jpg',
+            encoding: '7bit',
+            mimetype: 'image/jpeg',
+            destination: '/tmp',
+            filename: '863a23eb110d177eb4a8305817dcfe81',
+            path: '/tmp/863a23eb110d177eb4a8305817dcfe81_processed',
+            size: 320585,
+            name: 'test.jpg',
+            type: 'image/jpeg',
+            ext: '.jpg'
+        },
+        space: {
+            fieldname: 'file',
+            originalname: 'test image.jpg',
+            encoding: '7bit',
+            mimetype: 'image/jpeg',
+            destination: '/tmp',
+            filename: '22a9cd23586cd2d323d854feddb3fa13',
+            path: '/tmp/22a9cd23586cd2d323d854feddb3fa13_processed',
+            size: 320585,
+            name: 'test image.jpg',
+            type: 'image/jpeg',
+            ext: '.jpg'
+        }
+    }
 });
