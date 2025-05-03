02d69dcbb71354156905b599baafe4a7f7ed0f48
Daniel Smith <dannmasta@gmail.com>
2025-05-02 20:39:30 -0700

    fix typo

--- index.mjs
+++ index.mjs
@@ -5,7 +5,7 @@ import { GCSAdapter } from './lib/adapter.mjs';
 // Unfortunately this means we can't route to .cjs files programatically,
 // and we have to set the packge type to commonjs for now
 // Hopefully ghost supports esm soon
-// Note: Ghost expects the adapter class to be module.export
+// Note: Ghost expects the adapter class to be module.exports
 // Note: Rollup can't mix named and default exports, so we can only export default
 // from the main entrypoint
 // https://nodejs.org/api/modules.html#all-together
