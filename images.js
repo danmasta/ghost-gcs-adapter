25e51d9a48a523bcbc5615b48193653a93e71ead
Daniel Smith <dannmasta@gmail.com>
2025-05-02 20:32:37 -0700

    add entrypoints for typed adapters

---
+++ images.js
@@ -0,0 +1,1 @@
+module.exports = require('./dist/cjs/type/images.cjs');
---
+++ type/files.mjs
@@ -0,0 +1,9 @@
+import { GCSAdapter, StorageAdapterBase } from '../lib/adapter.mjs';
+
+// Note: Fix Adapter does not inherit from base class limitation
+// https://github.com/TryGhost/Ghost/blob/6af4d8f01df657ad9ba425f7ac78b50d0088068d/ghost/core/core/server/services/adapter-manager/AdapterManager.js#L135
+export default class Files extends StorageAdapterBase {
+    constructor (opts) {
+        return new GCSAdapter({ ...opts, type: 'files' });
+    }
+}
